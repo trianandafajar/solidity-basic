6c27d49ef91cf5f9bf11c432556e89bfa262028c
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-17 10:20:33 +0700

    Update Migrations

--- Mappings/contracts/Migrations.sol
+++ Mappings/contracts/Migrations.sol
@@ -2,17 +2,19 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Migrations {
-  address public owner = msg.sender;
-  uint public last_completed_migration;
+  address public owner = msg.sender;  // The owner is set to the address that deploys the contract
+  uint public last_completed_migration;  // Tracks the last migration step completed
 
+  // Modifier to restrict certain functions to the contract owner
   modifier restricted() {
     require(
-      msg.sender == owner,
+      msg.sender == owner,  // Only allow the contract's owner to call the function
       "This function is restricted to the contract's owner"
     );
-    _;
+    _;  // Placeholder for function execution
   }
 
+  // This function records the migration step completed (helps in tracking which migration was last completed)
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
   }
