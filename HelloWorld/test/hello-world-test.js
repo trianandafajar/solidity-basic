4c0b788616b392ba3dd801cf2fa4fd665ff8f64c
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-17 10:16:22 +0700

    Update Comment

--- HelloWorld/test/hello-world-test.js
+++ HelloWorld/test/hello-world-test.js
@@ -1,9 +1,16 @@
 const HelloWorld = artifacts.require("HelloWorld");
 
-contract("HelloWorld", ()=> { 
-    it("Should create and execute Simple Smart Contract", async ()=> {
+contract("HelloWorld", () => { 
+    it("Should create and execute Simple Smart Contract", async () => {
         const helloWorld = await HelloWorld.deployed();
+        
+        // Call the `hello()` function of the contract
         const result = await helloWorld.hello();
-        console.log("message : ",result);
+        
+        // Log the result for debugging purposes (optional)
+        console.log("Message:", result);
+        
+        // Use assertion to verify the result
+        assert.equal(result, "Hello, World!", "The hello message should be 'Hello, World!'");
     });
 });
