3401d9e1f48cfa02d38b3f619576b71f4f89660d
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-17 10:16:55 +0700

    Update Initial

--- HelloWorld/migrations/1_initial_migration.js
+++ HelloWorld/migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("HelloWorld");
+const HelloWorld = artifacts.require("HelloWorld");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(HelloWorld);  // Deploy the HelloWorld contract
 };
