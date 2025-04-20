a18d7827dcf717d80a71aa4e7f33d66609e5e5a8
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-20 23:15:46 +0700

    Update Test

--- HelloWorld/test/hello-world-test.js
+++ HelloWorld/test/hello-world-test.js
@@ -1,16 +1,17 @@
 const HelloWorld = artifacts.require("HelloWorld");
 
-contract("HelloWorld", () => { 
-    it("Should create and execute Simple Smart Contract", async () => {
-        const helloWorld = await HelloWorld.deployed();
-        
-        // Call the `hello()` function of the contract
-        const result = await helloWorld.hello();
-        
-        // Log the result for debugging purposes (optional)
-        console.log("Message:", result);
-        
-        // Use assertion to verify the result
-        assert.equal(result, "Hello, World!", "The hello message should be 'Hello, World!'");
-    });
+contract("HelloWorld", () => {
+  it("Should create and execute Simple Smart Contract", async () => {
+    // Deploy contract
+    const helloWorld = await HelloWorld.deployed();
+
+    // Panggil fungsi `hello()` dari kontrak
+    const result = await helloWorld.hello();
+
+    // Debug: log hasil ke terminal
+    console.log("Message:", result);
+
+    // Verifikasi hasil menggunakan assertion
+    assert.equal(result, "Hello, World!", "The hello message should be 'Hello, World!'");
+  });
 });
