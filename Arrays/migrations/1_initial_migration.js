894e768117047e492362d8bf46585fed0b1d529e
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-20 23:16:03 +0700

    Update Arrays Migrations

--- Arrays/migrations/1_initial_migration.js
+++ Arrays/migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function (deployer) {
+  // Deploy the Migrations contract to the blockchain
   deployer.deploy(Migrations);
 };
