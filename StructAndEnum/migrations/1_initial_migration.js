745820bddeb614148c0de64f6768d4ecdad8209c
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-20 23:16:16 +0700

    Update Struct Migrations

--- StructAndEnum/migrations/1_initial_migration.js
+++ StructAndEnum/migrations/1_initial_migration.js
@@ -1,5 +1,8 @@
+// Ambil kontrak "Migrations" yang digunakan untuk mencatat status migrasi
 const Migrations = artifacts.require("Migrations");
 
+// Fungsi ini dipanggil oleh Truffle saat melakukan deploy kontrak
 module.exports = function (deployer) {
+  // Deploy kontrak Migrations ke jaringan blockchain
   deployer.deploy(Migrations);
 };
